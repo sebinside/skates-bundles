b4934b173688aa02f38e5c45d58b39ab825f59a9
sebinside <hi@sebinside.de>
2021-01-02 19:25:37 +0100

    Refactored nanoleaf test bundle.

--- nanoleaf-testing/extension/NanoleafClient.ts
+++ nanoleaf-testing/extension/NanoleafClient.ts
@@ -1,9 +1,16 @@
 import { NodeCG } from "nodecg/types/server";
 import fetch from 'node-fetch';
+import { response } from "express";
+
+export interface Color { red: number, green: number, blue: number }
 
 export class NanoleafClient {
- 
+
     private static defaultPort = 16021
+
+    private static buildBaseRequestAddress(ipAddress: string, authToken: string) {
+        return `http://${ipAddress}:${NanoleafClient.defaultPort}/api/v1/${authToken}`
+    }
 
     static async printAuthKey(ipAddress: string, nodecg: NodeCG) {
 
@@ -22,16 +29,43 @@ export class NanoleafClient {
             nodecg.log.warn(errorMessage)
         }
     }
- 
+
     static async verifyConnection(ipAddress: string, authToken: string) {
-        const response = await fetch(`http://${ipAddress}:${this.defaultPort}/api/v1/${authToken}`,
-                { method: 'GET' })
+        const response = await fetch(NanoleafClient.buildBaseRequestAddress(ipAddress, authToken),
+            { method: 'GET' })
         return response.status === 200
     }
 
+    static convertHSVtoRGB(color: { hue: number, saturation: number, value: number }): Color {
+        // based on: https://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+        let h = color.hue;
+        let s = color.saturation;
+        let v = color.value;
+        let r, g, b, i, f, p, q, t;
+        i = Math.floor(h * 6);
+        f = h * 6 - i;
+        p = v * (1 - s);
+        q = v * (1 - f * s);
+        t = v * (1 - (1 - f) * s);
+        switch (i % 6) {
+            case 0: r = v, g = t, b = p; break;
+            case 1: r = q, g = v, b = p; break;
+            case 2: r = p, g = v, b = t; break;
+            case 3: r = p, g = q, b = v; break;
+            case 4: r = t, g = p, b = v; break;
+            case 5: r = v, g = p, b = q; break;
+            default: r = 0, g = 0, b = 0; break;
+        }
+        return {
+            red: Math.round(r * 255),
+            green: Math.round(g * 255),
+            blue: Math.round(b * 255)
+        };
+    }
+
     constructor(private ipAddress: string, private authToken: string, nodecg: NodeCG) {
-        NanoleafClient.verifyConnection(ipAddress,authToken).then(response => {
-            if(response) {
+        NanoleafClient.verifyConnection(ipAddress, authToken).then(response => {
+            if (response) {
                 nodecg.log.info("Connected to Nanoleafs successfully!")
             } else {
                 nodecg.log.error("Unable to connect to Nanoleafs. Please check your credentials.")
@@ -39,8 +73,73 @@ export class NanoleafClient {
         })
     }
 
-    getAllPanelInfo() {
-
+    private async callGET(relativePath: String) {
+        return fetch(NanoleafClient.buildBaseRequestAddress(this.ipAddress, this.authToken) + relativePath,
+            { method: 'GET' })
     }
 
+    private async callPUT(relativePath: String, body: any) {
+        return fetch(NanoleafClient.buildBaseRequestAddress(this.ipAddress, this.authToken) + relativePath,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            })
+    }
+
+    async getAllPanelInfo() {
+        return this.callGET("")
+    }
+
+    async getAllPanelIDs() {
+        const response = await this.getAllPanelInfo()
+
+        if (response.status !== 200) {
+            return []
+        }
+
+        const json = await response.json();
+        const panelIDs = json.panelLayout?.layout?.positionData
+            ?.map((entry: any) => entry.panelId)
+            .filter((entry: number) => entry !== 0);
+
+        return panelIDs;
+    }
+
+    async setPanelColor(panelId: number, color: Color) {
+        this.setPanelColors([{ panelId: panelId, color: color }]);
+    }
+
+    async setPanelColors(data: { panelId: number, color: Color }[]) {
+        if (data.length >= 1) {
+            const animData = `${data.length}` +
+                data
+                    .map((entry) => ` ${entry.panelId} 1 ${entry.color.red} ${entry.color.green} ${entry.color.blue} 0 1`)
+                    .join("")
+
+            const json = {
+                "write":
+                {
+                    "command": "display",
+                    "animType": "static",
+                    "animData": animData,
+                    "loop": false,
+                    "palette": []
+                }
+            }
+
+            this.callPUT("/effects", json)
+        }
+    }
+
+    async setBrightness(level: number) {
+        const data = { brightness: { value: level } };
+        this.callPUT("/state/brightness", data);
+    }
+
+    // TODO: State, Hue, Saturation, Color Temperature
+    // TODO: getAllPanelColors, getPanelColor
+    // TODO: animatePanelColors(values, type: RANDOM, BOTTOMUP)
 }
