918b687fb47b2366e857d9172795c2359171c932
Extrem Techniker <extremtechniker15@gmail.com>
2021-12-17 23:05:26 +0100

    Add bot702 to SteamBar

--- stream-bar/extension/index.ts
+++ stream-bar/extension/index.ts
@@ -2,11 +2,13 @@ import { NodeCG } from "nodecg-types/types/server";
 import { requireService } from "nodecg-io-core";
 import { SpotifyServiceClient } from "nodecg-io-spotify";
 import { StreamElementsServiceClient } from "nodecg-io-streamelements";
+import { TwitchChatServiceClient } from "nodecg-io-twitch-chat";
 import { StreamBarManager } from "./StreamBarManager";
 
 module.exports = function (nodecg: NodeCG) {
     const spotifyService = requireService<SpotifyServiceClient>(nodecg, "spotify");
     const streamElementsService = requireService<StreamElementsServiceClient>(nodecg, "streamelements");
+    const twitchChatService = requireService<TwitchChatServiceClient>(nodecg, "twitch-chat");
 
-    new StreamBarManager(spotifyService, streamElementsService, nodecg);
+    new StreamBarManager(spotifyService, streamElementsService, twitchChatService, nodecg);
 };
